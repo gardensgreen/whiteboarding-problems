ba774cef563ea873505ef99a13b4c69b0621c9cd
Daniel TIllero <danieltillero67@gmail.com>
2020-09-28 18:38:17 -0400

    Implement two sum in O(n) time

--- whiteboarding-skeleton.js
+++ whiteboarding-skeleton.js
@@ -5,6 +5,29 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
 
+// Input = INT: []
+// Output = indices: []
+
+function twoSum(array, target) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length - 1; j++) {
+            if (array[i] + array[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+const twoSumFaster = function (nums, target) {
+    const comp = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (comp[nums[i]] >= 0) {
+            return [comp[nums[i]], i];
+        }
+        comp[target - nums[i]] = i;
+    }
+};
+
 // Example 1:
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
